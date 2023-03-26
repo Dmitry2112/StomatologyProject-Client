71668c9e88a87e6d72b71ec00ce215710763083c
Dmitry <dmitriy_kozachenko_02@mail.ru>
2023-03-26 21:39:54 +0500

    migrate from css to scss

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent {
 
