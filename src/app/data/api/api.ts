dc945fbe052c32976d434841c503e5e24758b3f9
Dmitry <dmitriy_kozachenko_02@mail.ru>
2023-06-15 11:27:15 +0500

    fix route for req

--- src/app/data/api/api.ts
+++ src/app/data/api/api.ts
@@ -1,2 +1,2 @@
-//export const apiUrl: string = 'https://restful-api-vercel-lake.vercel.app';
-export const apiUrl: string = 'http://localhost:3000';
+export const apiUrl: string = 'https://restful-api-vercel-lake.vercel.app';
+//export const apiUrl: string = 'http://localhost:3000';
